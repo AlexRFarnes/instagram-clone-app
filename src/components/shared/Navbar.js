cfb127fd5d1673fb005380294fbffafec06dd9db
Alex <alexrfarnes@gmail.com>
2021-08-03 22:44:03 +0800

    Add the search bar and icons components to the Navbar

--- src/components/shared/Navbar.js
+++ src/components/shared/Navbar.js
@@ -1,16 +1,35 @@
-import { AppBar } from "@material-ui/core";
+import { AppBar, Avatar, Hidden, InputBase } from "@material-ui/core";
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, useHistory } from "react-router-dom";
 import { useNavbarStyles } from "../../styles";
 import logo from "../../images/logo.png";
+import {
+  LoadingIcon,
+  AddIcon,
+  LikeIcon,
+  LikeActiveIcon,
+  ExploreIcon,
+  ExploreActiveIcon,
+  HomeIcon,
+  HomeActiveIcon,
+} from "../../icons";
+import { defaultCurrentUser } from "../../data";
 
-function Navbar() {
+function Navbar({ minimalNavbar }) {
   const classes = useNavbarStyles();
+  const history = useHistory();
+  const path = history.location.pathname;
 
   return (
     <AppBar className={classes.appBar}>
       <section className={classes.section}>
         <Logo />
+        {!minimalNavbar && (
+          <>
+            <Search />
+            <Links path={path} />
+          </>
+        )}
       </section>
     </AppBar>
   );
@@ -25,6 +44,74 @@ function Logo() {
           <img src={logo} alt='Instagram' className={classes.logo} />
         </div>
       </Link>
+    </div>
+  );
+}
+
+function Search() {
+  const classes = useNavbarStyles();
+  const [query, setQuery] = React.useState("");
+
+  let loading = false;
+
+  function handleClearInput() {
+    setQuery("");
+  }
+
+  return (
+    <Hidden xsDown>
+      <InputBase
+        className={classes.input}
+        value={query}
+        onChange={event => setQuery(event.target.value)}
+        startAdornment={<span className={classes.searchIcon} />}
+        endAdornment={
+          loading ? (
+            <LoadingIcon />
+          ) : (
+            <span onClick={handleClearInput} className={classes.clearIcon} />
+          )
+        }
+        placeholder='Search'
+      />
+    </Hidden>
+  );
+}
+
+function Links({ path }) {
+  const classes = useNavbarStyles();
+  const [showList, setList] = React.useState(false);
+
+  function handleToggleList() {
+    setList(prev => !prev);
+  }
+
+  return (
+    <div className={classes.linksContainer}>
+      <div className={classes.linksWrapper}>
+        <Hidden xsDown>
+          <AddIcon />
+        </Hidden>
+        <Link to='/'>{path === "/" ? <HomeActiveIcon /> : <HomeIcon />}</Link>
+        <Link to='/explore'>
+          {path === "/explore" ? <ExploreActiveIcon /> : <ExploreIcon />}
+        </Link>
+        <div className={classes.notifications} onClick={handleToggleList}>
+          {showList ? <LikeActiveIcon /> : <LikeIcon />}
+        </div>
+        <Link to={`/${defaultCurrentUser.username}`}>
+          <div
+            className={
+              path === `/${defaultCurrentUser.username}`
+                ? classes.profileActive
+                : ""
+            }></div>
+          <Avatar
+            src={defaultCurrentUser.profile_image}
+            className={classes.profileImage}
+          />
+        </Link>
+      </div>
     </div>
   );
 }
